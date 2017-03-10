4ccef00b2e4057d7bd068a6ca60035a59df7000e
chjo0027 <chjo0027@gmail.com>
2017-03-10 21:31:26 +0100

    Lagt till tillbaka lämning

--- students/views.py
+++ students/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from django.views.generic import View
 from students.forms import StudentForm
 from students.models import Students
+from books.forms import GetStudentForm
+from books.models import Books, Borrowship
+from django.contrib import messages
 
 
 def index(request):
@@ -35,3 +38,54 @@ class AddStudent(View):
             return render(request, "add_student.html", {"form": form, "message": "Student tillagd"})
 
         return render(request, "add_student.html", {"form": form})
+
+class StudentView(View):
+
+    FORM_CLASS = GetStudentForm
+
+    def get(self, request, pk, *args, **kwargs):
+        students = Students.objects.all().order_by("lastname")
+        form = self.FORM_CLASS()
+
+        student = Students.objects.get(pk=pk)
+        try:
+            loans = Borrowship.objects.filter(student=Students(pk), returned=False)
+            books = []
+            for loan in loans:
+                book = Books.objects.get(pk=loan.book.pk)
+                books.append(book)
+        except Borrowship.DoesNotExist:
+            books = []
+
+        return render(request, "student.html", {"students": students,
+                                                "student": student,
+                                                "books": books})
+
+    def post(self, request, pk):
+        students = Students.objects.all().order_by("lastname")
+        student = Students.objects.get(pk=pk)
+        book = request.POST.get('book')
+
+        try:
+            loan = Borrowship.objects.get(book=Books(book), student=Students(pk), returned=False)
+            loan.returned = True
+            book = Books.objects.get(pk=book)
+            book.amount += 1
+            book.save()
+            loan.save()
+
+        except Borrowship.DoesNotExist:
+            pass
+
+        try:
+            loans = Borrowship.objects.filter(student=Students(pk), returned=False)
+            books = []
+            for loan in loans:
+                book = Books.objects.get(pk=loan.book.pk)
+                books.append(book)
+        except Borrowship.DoesNotExist:
+            books = []
+
+        return render(request, "student.html", {"students": students,
+                                             "student": student,
+                                             "books": books,})
