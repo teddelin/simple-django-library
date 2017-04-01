2f7201750ab79fbe6848f91b0bab386f706b8856
Ted Johansson <tedjohanssondeveloper@gmail.com>
2017-04-01 21:44:56 +0100

    Change index

--- simpleDjangoLibrary/settings.py
+++ simpleDjangoLibrary/settings.py
@@ -11,9 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-from datetime import timedelta
 
-from celery.schedules import crontab
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
